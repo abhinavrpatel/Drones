02a11f278accfc6ae00f8241f29b19a9ed70decc
amipatel <abhinavrpatel@gmail.com>
2017-04-10 19:43:48 -0700

    error handling for client POST complete

--- src/blockchain/drones/ClientServlet.java
+++ src/blockchain/drones/ClientServlet.java
@@ -24,16 +24,32 @@ public class ClientServlet extends HttpServlet {
         String userID = request.getParameter(ARG_USER);
         String padID = request.getParameter(ARG_PAD);
         String powerExpected = request.getParameter(ARG_EXPECTED);
+        double power;
+        try {
+            power = Double.valueOf(powerExpected);
+        } catch (NumberFormatException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameters given");
+            return;
+        }
+
+        if (userID == null || padID == null || powerExpected == null
+                || userID.equals("") || padID.equals("") || power <= 0) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameters given");
+            return;
+        }
 
         DroneClient drone = DroneDB.loadDroneClient(userID);
         ChargingPad pad = DroneDB.loadChargingPad(padID);
-        Transaction transaction = new Transaction(drone, pad, Double.valueOf(powerExpected));
+        Transaction transaction = new Transaction(drone, pad, power);
 
         if (transaction.begin()) {
             Cache.add(transaction);
-            // TODO: write okay back to response
+            response.setStatus(HttpServletResponse.SC_CREATED);
         } else {
-            // TODO: return error complaint
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "We were unable to create your transaction");
         }
     }
 
